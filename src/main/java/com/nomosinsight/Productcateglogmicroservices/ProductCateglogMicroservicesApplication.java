4eb5a2e6f94dbcd381e7da6406d90c6c79535968
jacker0031 <jenish31vekariy@gmail.com>
2024-03-13 13:35:07 +0530

    Initial Commit

---
+++ src/main/java/com/nomosinsight/Productcateglogmicroservices/ProductCateglogMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.nomosinsight.Productcateglogmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCateglogMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCateglogMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nomosinsight/Productcateglogmicroservices/ProductCateglogMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomosinsight.Productcateglogmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCateglogMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
